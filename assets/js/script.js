02a819db3f84c16f0834d2bc5a95b759022d3f74
Landrell Williams <dontess5@yahoo.com>
2021-09-26 21:21:58 -0400

    weather files update

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+var ApiKey = '945c64abb8a421a02d4a649310f9c495';
+var city;
+var queryUrl = 'api.openweathermap.org/data/2.5/weather?q=Atlanta&appid=2768f448218ea70934d7e1e405f20405';
+fetch(queryUrl)
